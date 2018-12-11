5f88f9179605979dd24eb8f29ee3d427c309f4a2
Martin Mihálik <34107535+mmihalik@users.noreply.github.com>
2018-12-11 14:05:27 +0100

    Improved build script

--- build-dist.sh
+++ build-dist.sh
@@ -2,20 +2,23 @@
 
 # Vytvorí zipko s nastaveným serverom a skriptom čo ho spustí na 64-bitových? linuxoch
 
-mkdir dist
-cp -r competition matboj manage.py requirements.txt dist/
-cd dist
+cd "$(dirname "$0")"
 
-rm -r competition/__pycache__
-rm -r competition/migrations/*
+DIST_NAME="server"
+ENV_NAME="pyenv"
+
+mkdir $DIST_NAME
+cp -r competition matboj manage.py requirements.txt $DIST_NAME/
+cd $DIST_NAME
+
+rm -r competition/__pycache__ competition/migrations/* matboj/__pycache__
 touch competition/migrations/__init__.py
-rm -r matboj/__pycache__
 
-echo -e "#!/bin/bash\nsource environment/bin/activate\npython3 manage.py runserver 0.0.0.0:8000\ndeactivate" > run.sh
+echo -e "#!/bin/bash\ncd \"\$(dirname \"\$0\")\"\nsource "$ENV_NAME"/bin/activate\npython3 manage.py runserver 0.0.0.0:8000\ndeactivate" > run.sh
 chmod +x run.sh
 
-python3 -m virtualenv --python=$(which python3) environment
-source environment/bin/activate
+python3 -m virtualenv --python=$(which python3) $ENV_NAME
+source $ENV_NAME/bin/activate
 
 pip3 install -r requirements.txt
 
@@ -27,6 +30,6 @@ deactivate
 
 cd ..
 
-zip -r dist.zip dist
+zip -r $DIST_NAME.zip $DIST_NAME
 
-rm -r dist
+rm -r $DIST_NAME
